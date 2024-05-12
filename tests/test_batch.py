7bf0bc69b8eebcb26ac9301746621f2d701b04ec
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-05-12 18:34:54 -0300

    tests: refactor fixtures

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -1,34 +1,61 @@
+from typing import Callable
+
 import pytest
 
 from app.model import Batch, OrderLine
 
-
-@pytest.fixture
-def order_line() -> OrderLine:
-    sku = "SMALL-TABLE"
-    return OrderLine(sku, 2)
+MakeOrderLine = Callable[[str, int], OrderLine]
+MakeBatch = Callable[[str, int], Batch]
 
 
 @pytest.fixture
-def batch_reference() -> str:
-    return "adsasd21312"
+def sku() -> str:
+    return "SMALL-TABLE"
 
 
-def test_allocate_reduces_qty(batch_reference: str, order_line: OrderLine):
-    batch = Batch(batch_reference, order_line.sku, 20)
+@pytest.fixture
+def make_order_line() -> MakeOrderLine:
+    def _make_order_line(sku: str, qty: int) -> OrderLine:
+        order_line = OrderLine()
+        order_line.sku = sku
+        order_line.qty = qty
+        return order_line
+    return _make_order_line
+
+
+@pytest.fixture
+def make_batch() -> MakeBatch:
+    def _make_batch(sku: str, qty: int) -> Batch:
+        reference = "test-reference"
+        return Batch(reference, sku, qty)
+    return _make_batch
+
+
+def test_allocate_reduces_qty(
+        sku: str,
+        make_batch: MakeBatch,
+        make_order_line: MakeOrderLine) -> None:
+    batch = make_batch(sku, 20)
+    order_line = make_order_line(sku, 2)
     batch.allocate(order_line)
     assert batch.qty == 18
 
 
-def test_allocate_error_if_not_enough_qty(batch_reference: str,
-                                          order_line: OrderLine):
-    batch = Batch(batch_reference, order_line.sku, 1)
+def test_allocate_error_if_not_enough_qty(
+    sku: str, make_batch: MakeBatch, make_order_line: MakeOrderLine
+) -> None:
+    batch = make_batch(sku, 10)
+    order_line = make_order_line(sku, 15)
     with pytest.raises(Exception):
         batch.allocate(order_line)
 
 
-def test_dont_allocate_line_twice(batch_reference: str, order_line: OrderLine):
-    batch = Batch(batch_reference, order_line.sku, 10)
+def test_dont_allocate_line_twice(
+        sku: str,
+        make_batch: MakeBatch,
+        make_order_line: MakeOrderLine) -> None:
+    batch = make_batch(sku, 10)
+    order_line = make_order_line(sku, 2)
     batch.allocate(order_line)
     batch.allocate(order_line)
     assert batch.qty == 8
