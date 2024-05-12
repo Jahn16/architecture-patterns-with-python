0cb153e7294657094021f4ca15b6695bbe8a4ca9
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-05-12 19:56:21 -0300

    tests: add tests expecting persistency

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -16,7 +16,9 @@ def sku() -> str:
 @pytest.fixture
 def make_order_line() -> MakeOrderLine:
     def _make_order_line(sku: str, qty: int) -> OrderLine:
+        order_id = "order-id"
         order_line = OrderLine()
+        order_line.order_id = order_id
         order_line.sku = sku
         order_line.qty = qty
         return order_line
@@ -67,3 +69,25 @@ def test_cannot_allocate_if_sku_mismatch(
     order_line = make_order_line("another-sku", qty)
     with pytest.raises(Exception):
         batch.allocate(order_line)
+
+
+def test_can_only_deallocate_allocated_lines(
+        sku: str,
+        make_batch: MakeBatch,
+        make_order_line: MakeOrderLine) -> None:
+    qty = 10
+    batch = make_batch(sku, qty)
+    order_line = make_order_line(sku, qty)
+    with pytest.raises(Exception):
+        batch.deallocate(order_line)
+
+
+def test_cannot_allocate_same_order_twice(
+        sku: str,
+        make_batch: MakeBatch,
+        make_order_line: MakeOrderLine) -> None:
+    batch = make_batch(sku, 10)
+    order_line = make_order_line(sku, 5)
+    batch.allocate(order_line)
+    batch.allocate(order_line)
+    assert batch.available_qty == 5
