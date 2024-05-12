56c164901824f7a977c62f839fc6e966b3cdb60d
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-05-12 19:33:39 -0300

    feat: implement allocate function

--- app/model.py
+++ app/model.py
@@ -23,8 +23,18 @@ class Batch:
     def __init__(self, reference: str, sku: str, qty: int):
         self.reference = reference
         self.sku = sku
-        self.qty = qty
+        self.available_qty = qty
         self.eta = None
 
     def allocate(self, order_line: OrderLine) -> None:
-        pass
+        if self.available_qty < order_line.qty:
+            raise Exception(
+                f"Available quantity {self.available_qty} is less than "
+                f"order line quantity {order_line.qty}"
+            )
+        if self.sku != order_line.sku:
+            raise Exception(
+                f"Batch SKU {self.sku} does not match "
+                f"order line SKU {order_line.sku}"
+            )
+        self.available_qty -= order_line.qty
